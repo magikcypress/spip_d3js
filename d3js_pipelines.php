57712b4b7f0234dfb3a06b35f20a6eed3bba617c
magikcypress <cyp@rouquin.me>
2014-11-24 16:45:36 +0100

    corrction bugs table2d3js

--- d3js_pipelines.php
+++ d3js_pipelines.php
@@ -20,7 +20,6 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 function d3js_jquery_plugins($tableau) {
 		$tableau[] = 'javascript/d3.v3.min.js';
 		$tableau[] = 'javascript/d3pie.min.js';
-		$tableau[] = 'javascript/d3.tip.js';
 		$tableau[] = 'javascript/table2d3js.js';
 	return $tableau;
 }
