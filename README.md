a32fe3932d33fa77682d51f42a1bc6aa6461fc38
webmestre <webmestre@psk3236.oxyd.net>
2015-07-22 11:57:19 +0200

    Correction bazar v3.0

---
+++ README.md
@@ -0,0 +1,14 @@
+spip_d3js
+============
+
+Faire de bien joli graph avec d3js
+
+## Version 1.0.1
+
+* Retouches graphiques sur les graphes
+* Possibilité d'afficher des graphs à partir de tableaux SPIP
+* Ajout de d3legend
+
+## Version 1.0.0
+
+* Possibilité de faire des pie, bar et line avec des tableaux
---
+++ d3js_autorisations.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Définit les autorisations du plugin d3js
+ *
+ * @plugin     d3js
+ * @copyright  2015
+ * @author     cyp
+ * @licence    GNU/GPL
+ * @package    SPIP\D3js\Autorisations
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+
+/**
+ * Fonction d'appel pour le pipeline
+ * @pipeline autoriser 
+ *
+ */
+function d3js_autoriser(){}
+
+
+
+
+?>
